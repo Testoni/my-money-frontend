9012302f4aaa2295c099e5debf273ab8b915c614
Gabriel Testoni <gabriel.testoni@gmail.com>
2020-06-24 00:49:20 -0300

    configuring index and app components

---
+++ src/index.jsx
@@ -0,0 +1,5 @@
+import react from 'React'
+import ReactDom from 'react-dom'
+import App from './main/app'
+
+ReactDom.render(<App />, document.getElementById('app'))
---
+++ src/main/app.jsx
@@ -0,0 +1,7 @@
+import react from 'React'
+
+export default props => (
+    <div className='wrapper'>
+        <h1>App</h1>
+    </div>
+)
