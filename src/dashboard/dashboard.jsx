2d27768e48e029475ca9c41c256d6aff5a9e7538
Gabriel Testoni <gabriel.testoni@gmail.com>
2020-06-24 22:31:28 -0300

    content and contentheader component

--- src/dashboard/dashboard.jsx
+++ src/dashboard/dashboard.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export default props => (
-    <div>
-        <h1>Dashboard</h1>
-    </div>
-)
+import ContentHeader from '../commom/template/contentHeader'
+import Content from '../commom/template/content'
+
+class Dashboard extends Component {
+    render() {
+        return (
+            <div>
+                <ContentHeader title='Dashboard' small='Version 1.0' />
+                <Content>
+                    Dashboard
+                </Content>
+            </div>
+        )
+    }
+}
+
+export default Dashboard
