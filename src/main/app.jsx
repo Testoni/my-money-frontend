f8885cdeb105e646bf95a60be830168969f5de37
Gabriel Testoni <gabriel.testoni@gmail.com>
2020-06-24 22:04:39 -0300

    react router

--- src/main/app.jsx
+++ src/main/app.jsx
@@ -4,13 +4,14 @@ import react from 'React'
 import Header from '../commom/template/header'
 import Sidebar from '../commom/template/siderbar'
 import Footer from '../commom/template/footer'
+import Routes from './routes'
 
 export default props => (
     <div className='wrapper'>
         <Header />
         <Sidebar />
         <div className='content-wrapper'>
-            <h1>Conteudo</h1>
+            <Routes />
         </div>
         <Footer />
     </div>
