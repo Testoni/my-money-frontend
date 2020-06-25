cd425d86634dcfa7b53ce7458b02866fe6442a5c
Gabriel Testoni <gabriel.testoni@gmail.com>
2020-06-24 21:41:46 -0300

    footer component

--- src/main/app.jsx
+++ src/main/app.jsx
@@ -3,10 +3,15 @@ import react from 'React'
 
 import Header from '../commom/template/header'
 import Sidebar from '../commom/template/siderbar'
+import Footer from '../commom/template/footer'
 
 export default props => (
     <div className='wrapper'>
         <Header />
         <Sidebar />
+        <div className='content-wrapper'>
+            <h1>Conteudo</h1>
+        </div>
+        <Footer />
     </div>
 )
