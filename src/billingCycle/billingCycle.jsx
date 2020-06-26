12acdef67fb90747778ad3d09cb0d04a3e08a3d5
Gabriel Testoni <gabriel.testoni@gmail.com>
2020-06-26 16:42:22 -0300

    convert to class billingCycle component

--- src/billingCycle/billingCycle.jsx
+++ src/billingCycle/billingCycle.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export default props => (
-    <div>
-        <h1>Billing Cycle</h1>
-    </div>
-)
+import ContentHeader from '../commom/template/contentHeader'
+import Content from '../commom/template/content'
+
+class BillinbCycle extends Component {
+    render() {
+        return (
+            <div>
+                <ContentHeader title='Ciclos de Pagamentos' small='Cadastro' />
+                <Content>
+                    Ciclos de Pagamentos
+                </Content>
+            </div>
+        )
+    }
+}
+
+export default BillinbCycle
