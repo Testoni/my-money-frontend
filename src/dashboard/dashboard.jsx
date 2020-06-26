9f27f78f6be2dde37154599c1d4731ea6bfc86d8
Gabriel Testoni <gabriel.testoni@gmail.com>
2020-06-26 14:53:20 -0300

    using valuebox and row in dashboard

--- src/dashboard/dashboard.jsx
+++ src/dashboard/dashboard.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 
 import ContentHeader from '../commom/template/contentHeader'
 import Content from '../commom/template/content'
+import ValueBox from '../commom/widget/valueBox'
+import Row from '../commom/layout/row'
 
 class Dashboard extends Component {
     render() {
@@ -9,7 +11,11 @@ class Dashboard extends Component {
             <div>
                 <ContentHeader title='Dashboard' small='Version 1.0' />
                 <Content>
-                    Dashboard
+                    <Row>
+                        <ValueBox cols='12 4' color='green' icon='bank' value='R$ 10' text='Todal de Créditos' />
+                        <ValueBox cols='12 4' color='red' icon='credit-card' value='R$ 10' text='Todal de Débitos' />
+                        <ValueBox cols='12 4' color='blue' icon='money' value='R$ 0' text='Valor Consolidado' />
+                    </Row>
                 </Content>
             </div>
         )
