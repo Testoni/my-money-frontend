5552d74b4c8cc42d36d79cc405f9d3030dd90bfb
Gabriel Testoni <gabriel.testoni@gmail.com>
2020-06-26 18:16:22 -0300

    create tab content component

--- src/billingCycle/billingCycle.jsx
+++ src/billingCycle/billingCycle.jsx
@@ -6,6 +6,7 @@ import Tabs from '../commom/tab/tabs'
 import TabsHeader from '../commom/tab/tabsHeader'
 import TabsContent from '../commom/tab/tabsContent'
 import TabHeader from '../commom/tab/tabHeader'
+import TabContent from '../commom/tab/tabContent'
 
 class BillinbCycle extends Component {
     render() {
@@ -21,7 +22,18 @@ class BillinbCycle extends Component {
                             <TabHeader label='Excluir' icon='trash-o' target='tabDelete' />
                         </TabsHeader>
                         <TabsContent>
-
+                            <TabContent id='tabList'>
+                                <h1>Lista</h1>
+                            </TabContent>
+                            <TabContent id='tabCreate'>
+                                <h1>Incluir</h1>
+                            </TabContent>
+                            <TabContent id='tabUpdate'>
+                                <h1>Alterar</h1>
+                            </TabContent>
+                            <TabContent id='tabDelete'>
+                                <h1>Excluir</h1>
+                            </TabContent>
                         </TabsContent>
                     </Tabs>
                 </Content>
