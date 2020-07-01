ab179fa3d8ab2a774b36ea9dc2861e2165f79c2f
Gabriel Testoni <gabriel.testoni@gmail.com>
2020-07-01 14:25:33 -0300

    Input component creditlist

--- src/billingCycle/billingCycleForm.jsx
+++ src/billingCycle/billingCycleForm.jsx
@@ -4,7 +4,7 @@ import { bindActionCreators } from 'redux'
 import { reduxForm, Field } from 'redux-form'
 
 import { init } from './billingCycleActions'
-import labelAndInput from '../commom/form/labelAndInput'
+import LabelAndInput from '../commom/form/labelAndInput'
 import CreditList from './creditList'
 
 class BillingCycleForm extends Component {
@@ -13,10 +13,10 @@ class BillingCycleForm extends Component {
         return (
             <form role='form' onSubmit={handleSubmit} >
                 <div className='box-body'>
-                    <Field name='name' component={labelAndInput} label='Nome' cols='12 4' placeholder='Informe o nome' readOnly={readOnly} />
-                    <Field name='month' component={labelAndInput} type='number' label='Mês' cols='12 4' placeholder='Informe o mês' readOnly={readOnly} />
-                    <Field name='year' component={labelAndInput} type='number' label='Ano' cols='12 4' placeholder='Informe o ano' readOnly={readOnly} />
-                    <CreditList cols='12 6' />
+                    <Field name='name' component={LabelAndInput} label='Nome' cols='12 4' placeholder='Informe o nome' readOnly={readOnly} />
+                    <Field name='month' component={LabelAndInput} type='number' label='Mês' cols='12 4' placeholder='Informe o mês' readOnly={readOnly} />
+                    <Field name='year' component={LabelAndInput} type='number' label='Ano' cols='12 4' placeholder='Informe o ano' readOnly={readOnly} />
+                    <CreditList cols='12 6' readOnly={readOnly} />
                 </div>
                 <div className='box-footer'>
                     <button type='submit' className={`btn btn-${this.props.submitClass}`}>{this.props.submitLabel}</button>
